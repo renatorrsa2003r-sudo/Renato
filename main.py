e272e4ff086a9a45f624c48738482baa7b198fe5
Renato <renatorrsa2003r@gmail.com>
2026-08-18 11:39:51 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,7 @@
+def subtrair_e_multiplicar_por_dois(a, b):
+    return (a - b) * 2
+
+if __name__ == "__main__":
+    print("Olá Mundo")
+    resultado = subtrair_e_multiplicar_por_dois(10, 4)
+    print(f"Resultado: {resultado}")
